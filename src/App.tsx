159a2f809742b3471c182a535abe07e5f3ceaa1d
Felipe Moura <felipenascimento.moura41@gmail.com>
2026-01-21 22:35:03 -0300

    feat: integrate TanStack Query for data fetching and add Axios HTTP client

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { Router } from "./Router/Router"
+const queryClient = new QueryClient()
 
 export function App() {
-  return <Router />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
 }
